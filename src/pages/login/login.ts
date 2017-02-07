a8f98912b7a90394fafaf60e5042d210d1c3ee80
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2017-02-07 22:41:46 +0100

    (fix) updated login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { NavController, App } from 'ionic-angular';
-
-import { TabsPage } from '../tabs/tabs';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  login: {username?: string, password?: string} = {};
-  submitted = false;
 
-  constructor(private navCtrl: NavController) {
+  constructor() {
   }
 
-  onLogin(form: NgForm) {
-    this.submitted = true;
-
-    if (form.valid) {
-      this.navCtrl.push(TabsPage);
-    }
-  }
 
 }
