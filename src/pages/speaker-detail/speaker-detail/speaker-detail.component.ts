a0a01cadbd76c7aca167f549b64d6e1bf4e91139
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2017-02-08 08:32:26 +0100

    (fix) stopped fix/infinite-navigation

--- src/pages/speaker-detail/speaker-detail/speaker-detail.component.ts
+++ src/pages/speaker-detail/speaker-detail/speaker-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Speaker, Session } from '../../../shared/entities';
+import { NavController, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'speaker-detail',
@@ -10,4 +11,20 @@ export class SpeakerDetailComponent {
   @Input() speaker: Speaker;
   @Output() url = new EventEmitter<string>();
   @Output() session = new EventEmitter<Session>();
+
+  showSessions = true;
+
+  constructor(private navController: NavController,
+              private viewController: ViewController) {
+
+  }
+
+  ngAfterViewInit() {
+    this.showSessions = this.navController.indexOf(this.viewController) < 4;
+  }
+
+  ionViewDidLoad() {
+    // doesn't work..
+    // this.showSessions = this.navController.indexOf(this.viewController) < 4;
+  }
 }
